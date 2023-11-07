23ce25a493e6e442504df34a87119f0045d09b63
PolyMelo <135617847+PolyMelo@users.noreply.github.com>
2023-11-06 23:02:20 -0300

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -2,11 +2,11 @@
 int main() {
     int tamanhoVetor = 10;
     int valores[tamanhoVetor];
-    for (int i = 0; i < tamanhoVetor; i++) {
+    int i;
+    for (i = 0; i < tamanhoVetor; i++) {
         scanf("%d", &valores[i]);
     }
-    printf("\n");
-    for (int i = 0; i < tamanhoVetor; i++) {
+    for (i = 0; i < tamanhoVetor; i++) {
         printf("%d", valores[i]);
         if (i < (tamanhoVetor - 1)) {
             printf(" ");
@@ -16,20 +16,19 @@ int main() {
     do{
         tamanhoVetor--;
         int novosValores[tamanhoVetor];
-        for(int i = 0; i < tamanhoVetor; i++){
+        for(i = 0; i < tamanhoVetor; i++){
             novosValores[i] = valores[i] + valores[i+1];
         }
-        for (int i = 0; i < tamanhoVetor; i++) {
+        for (i = 0; i < tamanhoVetor; i++) {
             printf("%d", novosValores[i]);
             if (i < (tamanhoVetor - 1)) {
                 printf(" ");
             }
         }
         printf("\n");
-        for(int i = 0; i < tamanhoVetor; i++){
+        for(i = 0; i < tamanhoVetor; i++){
             valores[i] = novosValores[i];
         }
     }while(tamanhoVetor > 1);
-    printf("\n");
     return 0;
 }
